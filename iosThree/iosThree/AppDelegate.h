f37f51c388fda786790ece4a0a058f6211592364
甘明强 <>
2018-11-15 15:03:06 +0800

    nihao

---
+++ iosThree/iosThree/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iosThree
+//
+//  Created by 甘明强 on 2018/11/15.
+//  Copyright © 2018年 com.zhihundaohe.abc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iosThree/iosThree/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosThree
+//
+//  Created by 甘明强 on 2018/11/15.
+//  Copyright © 2018年 com.zhihundaohe.abc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
